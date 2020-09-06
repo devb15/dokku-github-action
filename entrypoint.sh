96426685538182d370bbe599faa36ad9f4fc6db2
devb15 <34093122+devb15@users.noreply.github.com>
2020-09-06 20:47:20 +0530

    added ports

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,8 +29,10 @@ if [ -n "$FORCE_DEPLOY" ]; then
     GIT_COMMAND="$GIT_COMMAND --force"
 fi
 
+ssh dokku@apps.mobird.in proxy:ports $PROJECT
+
 echo "The deploy is starting"
 # Push to Dokku git repository
 GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" $GIT_COMMAND
 
-ssh dokku@apps.mobird.in proxy:ports $PROJECT
+
